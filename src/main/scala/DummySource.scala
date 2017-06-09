5f232dd18bf620c61ad0a36699e56a898f97c688
Lionel Blondé <lionel.blonde@gmail.com>
2017-06-10 00:09:54 +0200

    Add data generator for concept drift detection

--- src/main/scala/DummySource.scala
+++ src/main/scala/DummySource.scala
@@ -10,16 +10,37 @@ class DummySource extends Receiver[Int](org.apache.spark.storage.StorageLevel.ME
 
   // Start the thread that receives data over a connection
   def onStart() {
-    new Thread("Dummy Source") { override def run() { receive() } }.start()
+    new Thread("Dummy Source") {
+      override def run() { receive() }
+    }.start()
   }
 
   def onStop() {  }
 
   // Periodically generate a random number from 0 to 9, and the timestamp
-  private def receive() {
+  def receive() {
+    val gen = new Generator
     while(!isStopped()) {
-      store(Iterator(scala.util.Random.nextInt(10)))
+      store(gen.next())
       Thread.sleep(200)
+    }
+  }
+}
+
+class Generator0 extends Iterator[Double] {
+  def hasNext(): Boolean = {
+    // nothing fancy here: the generator is meant to run indefinitely
+    true
+  }
+  def next() = {
+    val drift: Long = 400L
+    val currentTime: Long = System.currentTimeMillis()/1000
+    if ((currentTime/drift) % 2) { // no need for floor, since both are Long
+      // we can fit an even number of drifts in the current time
+      10.0 + Random.nextGaussian()
+    } else {
+      // we can fit an odd number of drifts in the current time
+      -10.0 + Random.nextGaussian()
     }
   }
 }
